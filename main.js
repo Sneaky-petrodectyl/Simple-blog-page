5b247902e30a67a0e31dba5054028d31ffe2ffcd
Sneaky-petrodectyl <nileshjha412@gmail.com>
2017-10-21 08:30:19 +0530

    Done with entire application

---
+++ main.js
@@ -0,0 +1,26 @@
+document.getElementById('date').innerHTML = Date();
+
+var father = document.getElementById('father');
+father.onclick=function(){
+	document.getElementById('description').innerHTML = "<img src=\"https://cdn.images.yourquote.in/post/large/0/0/1/47/4Dro2192.jpg\" width = \"400px\">" ;
+}
+
+var hope = document.getElementById('hope');
+hope.onclick=function(){
+	document.getElementById('description').innerHTML = "<img src=\"https://cdn.images.yourquote.in/post/large/0/0/1/845/4q8X1336.jpg\" width = \"400px\">" ;
+}
+
+var dream = document.getElementById('dream');
+dream.onclick=function(){
+	document.getElementById('description').innerHTML = "<img src=\"https://cdn.images.yourquote.in/post/large/0/0/1/788/4XLO5288.jpg\" width = \"400px\">" ;
+}
+
+var expectation = document.getElementById('expectation');
+expectation.onclick=function(){
+	document.getElementById('description').innerHTML = "<img src=\"https://cdn.images.yourquote.in/post/large/0/0/2/399/x9LV3099.jpg\" width = \"400px\">" ;
+}
+
+var she = document.getElementById('she');
+she.onclick=function(){
+	document.getElementById('description').innerHTML = "<img src=\"https://cdn.images.yourquote.in/post/large/0/0/1/421/4dN34913.jpg\" width = \"400px\">" ;
+}
